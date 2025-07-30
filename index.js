c500b01c4ba8d4dd2197f00bb3d8f8325d3b687f
Ameer Hamza <ahamza292786@gmail.com>
2025-07-30 10:33:55 +0000

    project done, storing files in /uploads

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+var multer = require('multer');
 var app = express();
 
 app.use(cors());
@@ -11,8 +11,28 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const storage = multer.diskStorage({
+  destination: (req,file,cb) => {
+    cb(null, './uploads');
+  },
+  filename: (req,file,cb)=>{
+    cb(null,file.originalname);
+  }
+});
 
+const upload = multer({storage});
 
+app.post("/api/fileanalyse", upload.single('upfile'), (req,res)=>{
+  if (req.file){
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    })
+  } else {
+    res.json({error: 'File not uploaded'});
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
